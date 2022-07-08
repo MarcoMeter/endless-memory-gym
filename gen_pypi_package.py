cd81f18d621c134248855cf081147566b88a06f1
Marco Pleines <m.pleines@devbeyond.de>
2022-07-08 10:06:03 +0200

    gen pypi package

---
+++ gen_pypi_package.py
@@ -0,0 +1,48 @@
+import os
+from distutils.dir_util import copy_tree
+import shutil
+
+# Get the current working directory
+cwd = os.getcwd()
+
+# Create a new temporary directory for the package
+if not os.path.exists("tmp"):
+    os.makedirs("tmp")
+  
+# Copy the python files of the memory_gym package to the new directory
+copy_tree("memory_gym", dst="tmp\\memory_gym")
+
+# Optional: Add extra directories which should be included in the package  
+pypi_folders = []
+
+# Setup files to create the package
+setup_files = ["setup.py", "LICENSE", "README.md", "requirements.txt",]
+
+# Build the package directories
+for folder in pypi_folders:
+    copy_tree(folder, dst="tmp\\memory_gym\\" + folder)
+
+for file in setup_files:
+    shutil.copy(file, "tmp")
+
+# Deletes all pycache folders
+for dname, dirs, files in os.walk("tmp\\memory_gym"):
+    if "__pycache__" in dname:
+        shutil.rmtree(dname)
+
+# Build the package
+os.chdir("./tmp")
+os.system("python -m build")
+os.chdir(cwd)
+
+# Copy the package to the current directory
+copy_tree("tmp\\dist", "dist")
+
+# Clean up
+shutil.rmtree("tmp")
+
+# Upload the package to PyPI
+os.system("python -m twine upload dist/*")
+
+# Clean up
+shutil.rmtree("dist")
--- setup.py
+++ setup.py
@@ -1,7 +1,47 @@
-from setuptools import setup
+from setuptools import setup, find_packages
+import os
+import sys
+sys.path.insert(0, os.getcwd())
 
-setup(name="memory-gym",
+# Get current working directory
+cwd = os.getcwd()
+
+# Get install requirements from requirements.txt
+install_requires = None
+with open(cwd + "\\requirements.txt") as file:
+    install_requires = [module_name.rstrip() for module_name in file.readlines()]
+
+# Get long description from README.md
+long_description = ""
+with open(cwd + "\\README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+      name="memory-gym",
+      description="A gym that contains the memory benchmarks Mortar Mayhem, Mystery Maze and Searing Spotlights",
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      url="https://github.com/MarcoMeter/drl-memory-gym",
+      keywords = ["Deep Reinforcement Learning", "gym", "POMDP", "Imperfect Information", "Partial Observation"],
+      project_urls={
+            "Github": "https://github.com/MarcoMeter/drl-memory-gym",
+            "Bug Tracker": "https://github.com/MarcoMeter/drl-memory-gym/issues"
+      },
+      classifiers=[
+            "Programming Language :: Python :: 3",
+            "License :: OSI Approved :: MIT License",
+            "Operating System :: OS Independent",
+      ],
+      author="Marco Pleines",
+      package_dir={'': '.'},
+      packages=find_packages(where='.', include="memory_gym*"),
+      python_requires=">=3.6",
+      include_package_data=True,
+      install_requires=install_requires,
+      entry_points={
+            "console_scripts": [
+            "searing_spotlights=memory_gym.searing_spotlights.searing_spotlights:main",
+            ],
+      },
       version="0.0.1",
-      install_requires=["gym>=0.24.1",
-                        "pygame>=2.1.2"]
 )
