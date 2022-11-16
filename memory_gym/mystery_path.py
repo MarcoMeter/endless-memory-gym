fixed the visual initial rotation in Mystery Path

memory_gym/mystery_path.py
@@ -147,6 +147,7 @@ class MysteryPathEnv(gym.Env):
         # Setup the agent and sample its position
         rotation = self.np_random.choice([0, 45, 90, 135, 180, 225, 270, 315])
         self.agent = CharacterController(self.reset_params["agent_speed"], self.reset_params["agent_scale"], rotation)
+        self.rotated_agent_surface, self.rotated_agent_rect = self.agent.rotate(rotation)
         # Place the agent on the path's starting position
         self.agent.rect.center = (self.start[0] * self.tile_dim + self.agent.radius, self.start[1] * self.tile_dim + self.agent.radius)
         self.normalized_agent_position = self._normalize_agent_position(self.agent.rect.center)
@@ -154,7 +155,7 @@ class MysteryPathEnv(gym.Env):
         self.num_fails = 0
 
         # Draw
-        self._draw_surfaces([(self.path_surface, (0, 0)), (self.agent.surface, self.agent.rect)])
+        self._draw_surfaces([(self.path_surface, (0, 0)), (self.rotated_agent_surface, self.rotated_agent_rect)])
 
         # Retrieve the rendered image of the environment
         vis_obs = pygame.surfarray.array3d(pygame.display.get_surface()).astype(np.float32) / 255.0 # pygame.surfarray.pixels3d(pygame.display.get_surface()).astype(np.uint8)

memory_gym/mystery_path_grid.py
@@ -149,11 +149,12 @@ class GridMysteryPathEnv(gym.Env):
         start_pos = (self.start[0] * self.tile_dim + int(25 * SCALE), self.start[1] * self.tile_dim + int(25 * SCALE)) # TODO depend on agent radius and not hard coded int(25 * SCALE)
         self.normalized_agent_position = self._normalize_agent_position(start_pos)
         self.agent = GridCharacterController(SCALE, self.normalized_agent_position, self.mystery_path.to_grid(self.tile_dim), rotation)
+        self.rotated_agent_surface, self.rotated_agent_rect = self.agent.rotate(rotation)
         self.is_off_path = False
         self.num_fails = 0
 
         # Draw
-        self._draw_surfaces([(self.path_surface, (0, 0)), (self.agent.surface, self.agent.rect)])
+        self._draw_surfaces([(self.path_surface, (0, 0)), (self.rotated_agent_surface, self.rotated_agent_rect)])
 
         # Retrieve the rendered image of the environment
         vis_obs = pygame.surfarray.array3d(pygame.display.get_surface()).astype(np.float32) / 255.0 # pygame.surfarray.pixels3d(pygame.display.get_surface()).astype(np.uint8)